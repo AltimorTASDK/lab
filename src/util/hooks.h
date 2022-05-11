da278f5905fb23b14e69554964624e52346b5f39
AltimorTASDK <altimormc@gmail.com>
2022-05-11 03:31:35 -0400

    move macro newline backslashes to end of line

--- src/util/hooks.h
+++ src/util/hooks.h
@@ -9,23 +9,23 @@ struct hook_entry {
 	T hook;
 };
 
-#define HOOK(_function, ...) \
-	namespace CONCAT(_hook_, __COUNTER__) { \
-	static constexpr auto function = (_function); \
-	[[gnu::section(".hooks")]] [[gnu::used]] \
-	static hook_entry<decltype(function)> entry = { \
-		function, \
-		[]<typename ret, typename ...args>(ret(*)(args...)) { \
-			return [](args ...va) -> ret { \
-				const auto original = (ret(*)(args...))&entry; \
-				const auto lambda = (__VA_ARGS__); \
-				static_assert(requires { lambda(va...); }, \
-					"Wrong hook parameter types"); \
-				static_assert(std::is_same_v<decltype(lambda(va...)), ret>, \
-					"Wrong hook return type"); \
-				return lambda(va...); \
-			}; \
-		}(function) \
-	}; \
-	} \
+#define HOOK(_function, ...)                                                                       \
+	namespace CONCAT(_hook_, __COUNTER__) {                                                    \
+	static constexpr auto function = (_function);                                              \
+	[[gnu::section(".hooks")]] [[gnu::used]]                                                   \
+	static hook_entry<decltype(function)> entry = {                                            \
+		function,                                                                          \
+		[]<typename ret, typename ...args>(ret(*)(args...)) {                              \
+			return [](args ...va) -> ret {                                             \
+				const auto original = (ret(*)(args...))&entry;                     \
+				const auto lambda = (__VA_ARGS__);                                 \
+				static_assert(requires { lambda(va...); },                         \
+					"Wrong hook parameter types");                             \
+				static_assert(std::is_same_v<decltype(lambda(va...)), ret>,        \
+					"Wrong hook return type");                                 \
+				return lambda(va...);                                              \
+			};                                                                         \
+		}(function)                                                                        \
+	};                                                                                         \
+	}                                                                                          \
 	static_assert(true) // Force semicolon

d41053623fd173c7db3f57dc71c73ba0fd29dc22
AltimorTASDK <altimormc@gmail.com>
2022-05-20 02:42:39 -0400

    begin working on proper input action display

--- src/input/display.cpp
+++ src/input/display.cpp
@@ -2,57 +2,41 @@
 #include "os/os.h"
 #include "os/serial.h"
 #include "hsd/pad.h"
+#include "melee/action_state.h"
+#include "melee/player.h"
 #include "console/console.h"
 #include "imgui/draw.h"
 #include "input/poll.h"
+#include "player/events.h"
+#include "util/hooks.h"
 #include <cstring>
 #include <imgui.h>
 #include <ogc/machine/asm.h>
 
-static u32 poll_count[4];
-static u32 pressed_buttons[4];
-static u32 press_interval[4];
-static u32 last_press_time[4];
-static SIPadStatus last_status[4];
+static struct {
+	u32 poll_count;
+	u32 last_press_time[16];
+	SIPadStatus status;
+} port_state[4];
 
-static event_handler poll_handler(&events::input::poll, [](s32 chan, const SIPadStatus &status)
+EVENT_HANDLER(events::input::poll, [](s32 chan, const SIPadStatus &status)
 {
 	if (status.errstat != 0)
 		return;
 
-	const auto pressed = (status.buttons ^ last_status[chan].buttons) & status.buttons;
+	auto *state = &port_state[chan];
+	auto pressed = (status.buttons ^ state->status.buttons) & status.buttons;
 
-	if (pressed != 0) {
-		pressed_buttons[chan] = pressed;
-		press_interval[chan] = poll_count[chan] - last_press_time[chan];
-		last_press_time[chan] = poll_count[chan];
-
-		char buttons[8] = { '\0' };
-		if (pressed & Button_A)
-			strlcat(buttons, "A", sizeof(buttons));
-		if (pressed & Button_B)
-			strlcat(buttons, "B", sizeof(buttons));
-		if (pressed & Button_X)
-			strlcat(buttons, "X", sizeof(buttons));
-		if (pressed & Button_Y)
-			strlcat(buttons, "Y", sizeof(buttons));
-		if (pressed & Button_L)
-			strlcat(buttons, "L", sizeof(buttons));
-		if (pressed & Button_R)
-			strlcat(buttons, "R", sizeof(buttons));
-		if (pressed & Button_Z)
-			strlcat(buttons, "Z", sizeof(buttons));
-
-		OSContext context;
-		PPCMtmsr(PPCMfmsr() | MSR_FP);
-		OSSaveFPUContext(&context);
-
-		console::printf("% 7s %.01ff", buttons, (float)press_interval[chan] / Si.poll.y);
-
-		OSLoadFPUContext(&context);
-		PPCMtmsr(PPCMfmsr() & ~MSR_FP);
+	while (pressed != 0) {
+		const auto bit = __builtin_ctz(pressed);
+		state->last_press_time[bit] = state->poll_count;
+		pressed &= ~(1 << bit);
 	}
 
-	poll_count[chan]++;
-	last_status[chan] = status;
+	state->poll_count++;
+	state->status = status;
+});
+
+EVENT_HANDLER(events::player::as_change, [](Player *player, u32 old_state, u32 new_state)
+{
 });

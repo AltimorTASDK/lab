3bb2f2a8f6f5b136b4eb31b42d19b864c62fc4f1
AltimorTASDK <altimormc@gmail.com>
2022-05-22 23:43:13 -0400

    add utility to handle storing FPU state with RAII

---
+++ src/util/machine.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "os/context.h"
+#include "os/os.h"
+#include "util/preprocessor.h"
+#include <ogc/machine/asm.h>
+
+// Back up the FPU context and enable the FPU from an interrupt
+#define INTERRUPT_FPU_ENABLE() interrupt_fpu_save CONCAT(_interrupt_fpu_save_, __COUNTER__)
+
+class interrupt_fpu_save {
+	OSContext context;
+
+public:
+	interrupt_fpu_save()
+	{
+		PPCMtmsr(PPCMfmsr() | MSR_FP);
+		OSSaveFPUContext(&context);
+	}
+
+	~interrupt_fpu_save()
+	{
+		OSLoadFPUContext(&context);
+		PPCMtmsr(PPCMfmsr() & ~MSR_FP);
+	}
+};

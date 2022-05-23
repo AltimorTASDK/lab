e40156a05e71575389ea2a234bf655dd7fd29f28
AltimorTASDK <altimormc@gmail.com>
2022-05-23 05:27:13 -0400

    get basic action timing display working

--- src/input/display.cpp
+++ src/input/display.cpp
@@ -19,6 +19,7 @@
 
 constexpr auto INPUT_BUFFER_SIZE = MAX_POLLS_PER_FRAME * PAD_QNUM;
 constexpr auto ACTION_HISTORY = 10;
+constexpr auto ACTION_DISPLAY_TIME = 240;
 
 struct saved_input {
 	u8 qwrite;
@@ -34,8 +35,8 @@ struct processed_input {
 
 	processed_input(const Player *player, const SIPadStatus &status) :
 		buttons(status.buttons),
-		pressed((status.buttons ^ player->input.last_held_buttons) &  status.buttons),
-		released((status.buttons ^ player->input.last_held_buttons) & ~status.buttons),
+		pressed((status.buttons ^ player->input.held_buttons) &  status.buttons),
+		released((status.buttons ^ player->input.held_buttons) & ~status.buttons),
 		stick(convert_hw_coords(status.stick)),
 		cstick(convert_hw_coords(status.cstick))
 	{
@@ -44,36 +45,73 @@ struct processed_input {
 
 struct action_type {
 	const char *name;
+	// Action to time relative to
+	const action_type *base_action = nullptr;
 	// Predicate to detect state/inputs that trigger this action (before PlayerThink_Input)
-	bool(*input_predicate)(const Player *player, const processed_input &input);
+	// Returns 0 or an arbitrary mask corresponding to the input method (e.g. X/Y/Up for jump)
+	int(*input_predicate)(const Player *player, const processed_input &input);
 	// Predicate to detect whether the action succeeded (after PlayerThink_Input)
 	bool(*success_predicate)(const Player *player);
 };
 
 struct action {
 	const action_type *type;
-	u32 poll_num;
+	// Number of frames between input for this and base action
+	float frame_delta;
+	// Number of poll with input for this action
+	u32 poll_index;
+	// Video frame action was performed on
+	u32 frame;
 	bool success = false;
 	u8 port;
 };
 
-static const action_type action_types[] = {
-	{
-		.name = "Jump",
-		.input_predicate = [](const Player *player, const processed_input &input) {
-			if (player->airborne)
-				return false;
+namespace action_type_definitions {
 
-			return (input.pressed & (Button_X | Button_Y)) ||
-			       (input.stick.y >= plco->y_smash_threshold &&
-			        player->input.stick_y_hold_time < plco->y_smash_frames);
-		},
-		.success_predicate = [](const Player *player) {
-			return player->action_state == AS_KneeBend;
-		}
+static bool check_up_smash(const Player *player, const processed_input &input)
+{
+	return input.stick.y >= plco->y_smash_threshold &&
+	       player->input.stick_y_hold_time < plco->y_smash_frames;
+}
+
+static const auto jump = action_type {
+	.name = "Jump",
+	.input_predicate = [](const Player *player, const processed_input &input) {
+		if (player->airborne || player->action_state == AS_KneeBend)
+			return 0;
+
+		return ((input.pressed & Button_X)    ? (1 << 0) : 0) |
+		       ((input.pressed & Button_Y)    ? (1 << 1) : 0) |
+		       (check_up_smash(player, input) ? (1 << 2) : 0);
+	},
+	.success_predicate = [](const Player *player) {
+		return player->action_state == AS_KneeBend;
 	}
 };
 
+static const auto airdodge = action_type {
+	.name = "Jump -> Air Dodge",
+	.base_action = &jump,
+	.input_predicate = [](const Player *player, const processed_input &input) {
+		if (!player->airborne && player->action_state != AS_KneeBend)
+			return 0;
+
+		return ((input.pressed & Button_L)    ? (1 << 0) : 0) |
+		       ((input.pressed & Button_R)    ? (1 << 1) : 0);
+	},
+	.success_predicate = [](const Player *player) {
+		return player->action_state == AS_EscapeAir;
+	}
+};
+
+} // action_type_definitions
+
+static const action_type action_types[] = {
+	action_type_definitions::jump,
+	action_type_definitions::airdodge,
+};
+
+constexpr auto action_type_count = std::extent_v<decltype(action_types)>;
 static ring_buffer<saved_input, INPUT_BUFFER_SIZE> input_buffer[4];
 static ring_buffer<action, ACTION_HISTORY> action_buffer;
 static u32 last_confirmed_action[4];
@@ -124,6 +162,9 @@ EVENT_HANDLER(events::player::think::input::pre, [](Player *player)
 		return;
 	}
 
+	// Don't detect the same inputs for the same action repeatedly in one frame
+	u32 detected_inputs[action_type_count] = { 0 };
+
 	for (auto index = start_index; index <= end_index; index++) {
 		const auto *input = buffer.get(index);
 
@@ -132,14 +173,35 @@ EVENT_HANDLER(events::player::think::input::pre, [](Player *player)
 
 		const auto processed = processed_input(player, input->status);
 
-		for (const auto &type : action_types) {
-			if (!type.input_predicate(player, processed))
+		for (size_t type_index = 0; type_index < action_type_count; type_index++) {
+			const auto &type = action_types[type_index];
+			const auto mask = type.input_predicate(player, processed);
+
+			if ((mask & ~detected_inputs[type_index]) == 0)
 				continue;
 
+			detected_inputs[type_index] |= mask;
+
+			auto frame_delta = -1.f;
+
+			if (type.base_action != nullptr) {
+				for (size_t offset = 0; offset < action_buffer.stored(); offset++) {
+					const auto *action = action_buffer.head(offset);
+
+					if (action->type == type.base_action) {
+						const auto poll_delta = index - action->poll_index;
+						frame_delta = (float)poll_delta / Si.poll.y;
+						break;
+					}
+				}
+			}
+
 			action_buffer.add({
-				.type     = &type,
-				.poll_num = index,
-				.port     = player->port
+				.type        = &type,
+				.frame_delta = frame_delta,
+				.poll_index  = index,
+				.frame       = VIGetRetraceCount(),
+				.port        = player->port
 			});
 		}
 	}
@@ -156,7 +218,41 @@ EVENT_HANDLER(events::player::think::input::post, [](Player *player)
 	for (; last_confirmed_action[port] < action_buffer.count(); last_confirmed_action[port]++) {
 		auto *action = action_buffer.get(last_confirmed_action[port]);
 
-		if (action != nullptr && action->type->success_predicate(player))
+		if (action->type->success_predicate(player))
 			action->success = true;
 	}
+});
+
+EVENT_HANDLER(events::imgui::draw, []()
+{
+	ImGui::SetNextWindowPos({20, 20});
+	ImGui::Begin("Inputs", nullptr, ImGuiWindowFlags_NoResize
+	                              | ImGuiWindowFlags_AlwaysAutoResize
+	                              | ImGuiWindowFlags_NoMove
+	                              | ImGuiWindowFlags_NoInputs
+	                              | ImGuiWindowFlags_NoDecoration
+	                              | ImGuiWindowFlags_NoBackground);
+
+	ImGui::BeginTable("Inputs", 2, 0, {320, 480 - 40});
+
+	for (size_t offset = 0; offset < action_buffer.stored(); offset++) {
+		const auto *action = action_buffer.tail(offset);
+
+		if (VIGetRetraceCount() - action->frame > ACTION_DISPLAY_TIME)
+			continue;
+
+                ImGui::TableNextRow();
+		ImGui::TableNextColumn();
+                ImGui::TextUnformatted(action->type->name);
+
+		OSReport("drawing %s\n", action->type->name);
+		if (action->frame_delta != -1.f) {
+			ImGui::TableNextColumn();
+			ImGui::Text("%f", action->frame_delta);
+		}
+	}
+
+	ImGui::EndTable();
+
+	ImGui::End();
 });

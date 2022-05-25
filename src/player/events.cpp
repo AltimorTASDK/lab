5d7be1bae1d0237f09945bfeb8844a00135821fa
AltimorTASDK <altimormc@gmail.com>
2022-05-25 19:02:07 -0400

    pass old state to post input think event

--- src/player/events.cpp
+++ src/player/events.cpp
@@ -8,9 +8,7 @@ HOOK(Player_ASChange, [&](HSD_GObj *gobj, u32 new_state, u32 flags, HSD_GObj *pa
 {
 	auto *player = gobj->get<Player>();
 	const auto old_state = player->action_state;
-
 	original(gobj, new_state, flags, parent, start_frame, frame_rate, lerp_override);
-
 	events::player::as_change.fire(player, old_state, new_state);
 
 });
@@ -18,7 +16,8 @@ HOOK(Player_ASChange, [&](HSD_GObj *gobj, u32 new_state, u32 flags, HSD_GObj *pa
 HOOK(PlayerThink_Input, [&](HSD_GObj *gobj)
 {
 	auto *player = gobj->get<Player>();
+	const auto old_state = player->action_state;
 	events::player::think::input::pre.fire(player);
 	original(gobj);
-	events::player::think::input::post.fire(player);
+	events::player::think::input::post.fire(player, old_state);
 });
